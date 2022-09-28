a8a5957e5b3d9c827df2f64dbfeaf742e9f9141a
Sam <samjulian6@gmail.com>
2022-09-28 15:25:30 -0700

    Implemented rest of wordSearch() to find words horizontally and vertically

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,17 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    if (letters.length === 0) {
+        return false;
+      }
+    const horizontalJoin = letters.map((ls) => ls.join(""));
+    for (let l of horizontalJoin) {
+      if (l.includes(word)) return true;
     }
-}
+    const transpose = letters[0].map((val, colI) => letters.map((row) => row[colI]));
+    let verticalJoin = transpose.map((ls) => ls.join(""));
+    for (let l of verticalJoin) {
+        if (l.includes(word)) return true;
+      }
+    return false;
+  };
 
 module.exports = wordSearch
